231a331a63364fb739694d083a20713ffe3be4f8
Petras Malinauskas <petras.malinauskas@unity3d.com>
2020-11-19 21:25:23 +0200

    Initial Commit

---
+++ SquidPrint/SquidPrintApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquidPrintApp.swift
+//  SquidPrint
+//
+//  Created by Petras Malinauskas on 2020-11-19.
+//
+
+import SwiftUI
+
+@main
+struct SquidPrintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
