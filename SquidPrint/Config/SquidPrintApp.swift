fd60e294d0401639660a4936dcf15f44ea355ba1
Petras Malinauskas <petras.malinauskas@unity3d.com>
2020-11-21 15:56:30 +0200

    Setup XCGLogger

--- SquidPrint/Config/SquidPrintApp.swift
+++ SquidPrint/Config/SquidPrintApp.swift
@@ -6,6 +6,32 @@
 //
 
 import SwiftUI
+import XCGLogger
+
+let log: XCGLogger = {
+    // Create a logger object with no destinations
+    let log = XCGLogger(identifier: "advancedLogger", includeDefaultDestinations: false)
+
+    // Create a destination for the system console log (via NSLog)
+    let systemDestination = AppleSystemLogDestination(identifier: "advancedLogger.systemDestination")
+
+    // Optionally set some configuration options
+    systemDestination.outputLevel = .debug
+    systemDestination.showLogIdentifier = false
+    systemDestination.showFunctionName = true
+    systemDestination.showThreadName = true
+    systemDestination.showLevel = true
+    systemDestination.showFileName = true
+    systemDestination.showLineNumber = true
+    systemDestination.showDate = true
+
+    // Add the destination to the logger
+    log.add(destination: systemDestination)
+
+    // Add basic app info, version info etc, to the start of the logs
+    log.logAppDetails()
+    return log
+}()
 
 @main
 struct SquidPrintApp: App {
@@ -16,6 +42,13 @@ struct SquidPrintApp: App {
         WindowGroup {
             AppView(viewModel: appViewModel)
                 .environmentObject(appEnvironment)
+                .onAppear {
+                    log.info("App started")
+                }
         }
+    }
+    
+    private func setupLogger() {
+        
     }
 }
